55fd68038eea8f25fc8276034afe40c37f5bbcca
José Cruz <124106745+josejavierc13@users.noreply.github.com>
2023-11-12 02:01:40 -0400

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,82 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import java.awt.image.BufferedImage;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+public class Main extends JFrame {
+    private Image grassImage;
+    public Image Ducky;
+    public int circlex;
+    public int circley;
+
+    public Main() {
+        setTitle("BONK LE DUCK");
+        setSize(800, 800);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        try {
+            grassImage = ImageIO.read(new File("C:\\Users\\josej\\IdeaProjects\\BonkLeDuck\\IMAGES\\Grass_image.jpg"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
+        JPanel mainPanel = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                drawPond(g);
+                ducky(circlex,circley,g);
+
+            }
+        };
+
+        getContentPane().add(mainPanel);
+
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    private void drawPond(Graphics g) {
+        // Draw grass background
+        g.drawImage(grassImage, 0, 0, getWidth(), getHeight(), this);
+
+        // Draw water in the middle as a blue circle
+        int circleDiameter = 600;
+        int circleX = (getWidth() - circleDiameter) / 2;
+        int circleY = (getHeight() - circleDiameter) / 2 ;
+        System.out.println(circleX);
+        System.out.println(circleY);
+
+
+        g.setColor(new Color(10,  180, 255));
+        g.fillOval(circleX, circleY, circleDiameter, circleDiameter);
+
+        this.circlex = circleX;
+        this.circley = circleY;
+    }
+    private void ducky(int circleX, int circleY, Graphics g){
+        Random rand = new Random();
+
+        int randCoordinatesX = rand.nextInt(2000);
+        int randCoordinatesY = rand.nextInt(2000);
+        System.out.println(randCoordinatesX);
+
+
+        try {
+            BufferedImage bufferedImage = ImageIO.read(new File("C:\\Users\\josej\\IdeaProjects\\BonkLeDuck\\IMAGES\\DUCK.png"));
+            Image image = bufferedImage.getScaledInstance(20, 20, Image.SCALE_DEFAULT);
+            g.drawImage(image, randCoordinatesX + 600, randCoordinatesY + 600, 100, 100, this);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Main::new);
     }
 }
