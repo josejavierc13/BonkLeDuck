48363cac353ed340b235ee9d6ad18be486c1cbdc
José Cruz <124106745+josejavierc13@users.noreply.github.com>
2023-11-17 00:54:35 -0400

    Added Buttons for the duck and DEBUGGED

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,31 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.ImageObserver;
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Random;
 import java.awt.image.BufferedImage;
 
 
-public class Main extends JFrame {
+public class Main  {
     private final Image grassImage;
     public int CircleX;
     public int CircleY;
-    public int buttonCordX;
-    public int buttonCordY;
+
+    public JFrame mainFrame = new JFrame();
 
 
 
     public Main() {
-        setTitle("BONK LE DUCK");
-        setSize(800, 800);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(true);
+
+        mainFrame.setTitle("BONK LE DUCK");
+        mainFrame.setSize(800, 800);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setResizable(false);
 
         try {
             grassImage = ImageIO.read(new File("C:\\Users\\josej\\IdeaProjects\\BonkLeDuck\\IMAGES\\Grass_image.jpg"));
@@ -33,25 +38,28 @@ public class Main extends JFrame {
             protected void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 drawPond(g);
-                ducky(CircleX, CircleY,g);
+                ducky(CircleX, CircleY, g);
+
 
             }
         };
 
-        getContentPane().add(mainPanel);
+        mainFrame.setVisible(true);
+        mainFrame.add(mainPanel);
 
-        setLocationRelativeTo(null);
-        setVisible(true);
+
+
     }
+
 
     private void drawPond(Graphics g) {
         // Draw grass background
-        g.drawImage(grassImage, 0, 0, getWidth(), getHeight(), this);
+        g.drawImage(grassImage, 0, 0, mainFrame.getWidth(), mainFrame.getHeight(), (ImageObserver) this.mainFrame);
 
         // Draw water in the middle as a blue circle
         int circleDiameter = 600;
-        int circleX = (getWidth() - circleDiameter) / 2;
-        int circleY = (getHeight() - circleDiameter) / 2 ;
+        int circleX = (mainFrame.getWidth() - circleDiameter) / 2;
+        int circleY = (mainFrame.getHeight() - circleDiameter) / 2 ;
         System.out.println(circleX);
         System.out.println(circleY);
 
@@ -62,22 +70,32 @@ public class Main extends JFrame {
         this.CircleX = circleX;
         this.CircleY = circleY;
     }
-    private void ducky(int circleX, int circleY, Graphics g){
+    private void ducky(int circleX, int circleY, Graphics g) {
         Random rand = new Random();
 
-        int randCoordinatesX = rand.nextInt(2000);
-        int randCoordinatesY = rand.nextInt(2000);
+        int randCoordinatesX = rand.nextInt(500);
+        int randCoordinatesY = rand.nextInt(500);
         System.out.println(randCoordinatesX);
-        System.out.println(randCoordinatesY);
+        // System.out.println(randCoordinatesY);
 
 
-        try {
-            BufferedImage bufferedImage = ImageIO.read(new File("C:\\Users\\josej\\IdeaProjects\\BonkLeDuck\\IMAGES\\DUCK.png"));
-            Image image = bufferedImage.getScaledInstance(20, 20, Image.SCALE_DEFAULT);
-            g.drawImage(image, randCoordinatesX + 600, randCoordinatesY + 600, 100, 100, this);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        ImageIcon icon = new ImageIcon("C:\\Users\\josej\\IdeaProjects\\BonkLeDuck\\IMAGES\\DUCK.png");
+        Image image = icon.getImage(); // transform it
+        Image newimg = image.getScaledInstance(120, 120,  java.awt.Image.SCALE_SMOOTH);
+        icon = new ImageIcon(newimg);
+        JButton Button = new JButton(icon);
+        mainFrame.add(Button);
+
+
+        Button.setBounds(randCoordinatesX + 220, randCoordinatesY + 220, 100, 100);
+        Button.addActionListener(e -> {
+            Container parent = Button.getParent();
+            parent.remove(Button);
+            parent.revalidate();
+            parent.repaint();
+
+        });
+
 
     }
 
